e001c99f431b1e2bc4e7069c71ce621f5c783a76
falgoonit <falgoonit@gmail.com>
2017-01-29 18:28:35 -0500

    Grading Submission
    Written files for peer grading submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## File Includes 2 R functions
+## 
+## Functions included in this file: 
+## 1. makeCacheMatrix
+## 2. cacheSolve
 
-## Write a short comment describing this function
+## The pair of functions are intended to create a special 
+## "matrix" and cache its inverse. If the inverse has
+## already been calculated and the matrix has not changed,
+## the second function will return the inverse from cache.
+## Else, it will recalculate.
+
+
+## Define makeCacheMatrix function
+## Creates a special "matrix" object and cache inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Define cacheSolve function
+## Compute or return cached inverse of special "matrix"
+## created by makeCacheMatrix function defined above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+              message("getting cached data")
+              return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
